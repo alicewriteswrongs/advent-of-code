88d0a1adb602c81f27065a57d17e20a420b34c67
Alice Pote <alice.writes.wrongs@gmail.com>
2021-12-02 10:26:40 -0500

    day 1, part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,21 +1,42 @@
 use std::fs;
 
-pub fn solution() -> i32 {
-    let contents: Vec<i32> = fs::read_to_string("src/data/day_1.txt")
+fn get_data() -> Vec<i32> {
+    fs::read_to_string("src/data/day_1.txt")
         .unwrap()
         .trim()
         .split("\n")
         .map(|intstring| intstring.parse::<i32>().unwrap())
-        .collect();
+        .collect()
+}
 
-    contents
-        .iter()
-        .enumerate()
+fn count_increases(measurements: Vec<i32>) -> i32 {
+    measurements.iter().enumerate()
         .fold(0, |acc, (index, depth)| match index {
             0 => 0,
-            x => match contents[x - 1] < *depth {
+            x => match measurements[x - 1] < *depth {
                 true => acc + 1,
                 false => acc,
             },
         })
+}
+
+pub fn part_one() -> i32 {
+    let measurements: Vec<i32> = get_data();
+    count_increases(measurements)
+}
+
+pub fn part_two() -> i32 {
+    let measurements: Vec<i32> = get_data();
+
+    let mut idx: usize = 0;
+    let mut window_sums: Vec<i32> = vec![];
+
+    while idx + 2 < measurements.len() {
+        window_sums.push(
+            measurements[idx] + measurements[idx + 1] + measurements[idx + 2]
+        );
+        idx += 1;
+    };
+
+    count_increases(window_sums)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_1;
 
 fn main() {
-    println!("day 1 solution is {}", day_1::solution());
+    println!("day 1, part 1 solution is {}", day_1::part_one());
+    println!("day 1, part 2 solution is {}", day_1::part_two());
 }
