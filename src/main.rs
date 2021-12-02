a0455b1464173c26eb8eb57ebefb91779c121ab3
Alice Pote <alice.writes.wrongs@gmail.com>
2021-12-02 15:23:44 -0500

    day 2, part 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod day_1;
+mod day_2;
 
 fn main() {
     println!("day 1, part 1 solution is {}", day_1::part_one());
     println!("day 1, part 2 solution is {}", day_1::part_two());
+    println!("day 2, part 1 solution is {}", day_2::part_one());
 }
