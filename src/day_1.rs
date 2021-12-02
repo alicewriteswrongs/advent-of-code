7f63178a386ef3badac6527662a024c08e4cf803
Alice Pote <alice.writes.wrongs@gmail.com>
2021-12-02 10:42:57 -0500

    little refactor

--- src/day_1.rs
+++ src/day_1.rs
@@ -2,15 +2,16 @@ use std::fs;
 
 fn get_data() -> Vec<i32> {
     fs::read_to_string("src/data/day_1.txt")
-        .unwrap()
-        .trim()
-        .split("\n")
-        .map(|intstring| intstring.parse::<i32>().unwrap())
+        .expect("some issue reading the file :/")
+        .lines()
+        .map(|intstring| intstring.parse().unwrap())
         .collect()
 }
 
 fn count_increases(measurements: Vec<i32>) -> i32 {
-    measurements.iter().enumerate()
+    measurements
+        .iter()
+        .enumerate()
         .fold(0, |acc, (index, depth)| match index {
             0 => 0,
             x => match measurements[x - 1] < *depth {
@@ -27,16 +28,9 @@ pub fn part_one() -> i32 {
 
 pub fn part_two() -> i32 {
     let measurements: Vec<i32> = get_data();
-
-    let mut idx: usize = 0;
-    let mut window_sums: Vec<i32> = vec![];
-
-    while idx + 2 < measurements.len() {
-        window_sums.push(
-            measurements[idx] + measurements[idx + 1] + measurements[idx + 2]
-        );
-        idx += 1;
-    };
-
+    let window_sums = measurements
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect();
     count_increases(window_sums)
 }
