6c9efdb50a71ab7c3f8e49fbb29392a4039f7b94
Alice Pote <alice.writes.wrongs@gmail.com>
2021-12-08 16:28:54 -0500

    the first time it all compiled it worked correctly...rust is magical

--- src/day_4.rs
+++ src/day_4.rs
@@ -1,17 +1,36 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+struct Position {
+    number: u32,
+    // 'marked' means that this position has been marked as a drawn number already
+    marked: bool,
+}
+
+#[derive(Debug, Clone)]
 struct Board {
-    board: [u32; 25],
+    values: Vec<Position>,
 }
 
-#[derive(Debug)]
-struct Game {
-    moves: Vec<u32>,
-    boards: Vec<Board>,
+impl Board {
+    fn mark_number(self, number: u32) -> Board {
+        Board {
+            values: self
+                .values
+                .iter()
+                .map(|position| match number == position.number {
+                    true => Position {
+                        marked: true,
+                        ..(*position)
+                    },
+                    false => Position { ..(*position) },
+                })
+                .collect(),
+        }
+    }
 }
 
-fn get_data() -> Game {
+fn get_data() -> (Vec<u32>, Vec<Board>) {
     let raw_string =
         fs::read_to_string("src/data/day_4.txt").expect("some problem reading the file! :O");
 
@@ -19,32 +38,95 @@ fn get_data() -> Game {
         .split_once("\n\n")
         .expect("this should split just fine");
 
-    Game {
-        moves: raw_moves
-            .split(",")
-            .map(|numeral| numeral.parse().unwrap())
-            .collect(),
-        boards: raw_boards
-            .split("\n\n")
-            .map(|raw_board| Board {
-                board: raw_board
-                    .split("\n")
-                    .flat_map(|row| row.split(" "))
-                    .filter(|&str| str != "")
-                    .map(|numeral| numeral.parse().unwrap())
-                    .collect::<Vec<u32>>()
-                    .try_into()
-                    .expect("conversion to array panicked :("),
-            })
-            .collect(),
-    }
+    let moves = raw_moves
+        .split(",")
+        .map(|numeral| numeral.parse().unwrap())
+        .collect();
+
+    let boards = raw_boards
+        .split("\n\n")
+        .map(|raw_board| Board {
+            values: raw_board
+                .split("\n")
+                .flat_map(|row| row.split(" "))
+                .filter(|&str| str != "")
+                .map(|numeral| Position {
+                    number: numeral.parse().unwrap(),
+                    marked: false,
+                })
+                .collect(),
+        })
+        .collect();
+    (moves, boards)
+}
+
+fn mark_boards(boards: Vec<Board>, number: u32) -> Vec<Board> {
+    boards
+        .iter()
+        .map(|board| (*board).clone().mark_number(number))
+        .collect()
+}
+
+fn is_winning_board(board: &Board) -> bool {
+    // If all numbers in any row or any column of a board are marked, that board wins. (Diagonals
+    // don't count.)
+    //
+    // Here are the indices in a board:
+    //
+    // 0  1  2  3  4
+    // 5  6  7  8  9
+    // 10 11 12 13 14
+    // 15 16 17 18 19
+    // 20 21 22 23 24
+    //
+    // Any vertical or horizontal set is a winning set of indices.
+    let winning_indices: Vec<Vec<usize>> = vec![
+        (0..5).collect(),
+        (5..10).collect(),
+        (10..15).collect(),
+        (15..20).collect(),
+        (20..25).collect(),
+        vec![0, 5, 10, 15, 20],
+        vec![1, 6, 11, 16, 21],
+        vec![2, 7, 12, 17, 22],
+        vec![3, 8, 13, 18, 23],
+        vec![4, 9, 14, 19, 24],
+    ];
+
+    winning_indices
+        .iter()
+        .any(|indices| indices.iter().all(|&index| board.values[index].marked))
 }
 
 pub fn part_one() -> u32 {
-    let game = get_data();
+    let (moves, mut boards) = get_data();
 
-    println!("{:?}", game.moves);
-    println!("{:?}", game.boards);
+    for number in moves {
+        boards = mark_boards(boards, number);
+        let winning_boards: Vec<&Board> = boards
+            .iter()
+            .filter(|board| is_winning_board(board))
+            .collect();
 
+        if winning_boards.len() > 0 {
+            let winning_board = winning_boards[0];
+
+            // The score of the winning board can now be calculated. Start by finding the sum of
+            // all unmarked numbers on that board; in this case, the sum is 188. Then, multiply
+            // that sum by the number that was just called when the board won, 24, to get the final
+            // score, 188 * 24 = 4512.
+            let score: u32 =
+                winning_board
+                    .values
+                    .iter()
+                    .fold(0, |acc, position| match position.marked {
+                        true => acc,
+                        false => acc + position.number,
+                    });
+            return score * number;
+        }
+    }
+
+    // for 'type safety'
     32
 }
